b43267e5eda25e64d62dc0ec61b98740fd40e913
jucami90 <jucami90@gmail.com>
2025-11-10 22:59:26 -0500

    new git files

---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,35 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class UUIDv7Test {
+
+    @Test
+    public void shouldTestUUIDv7() {
+        UUID uuid1 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid2 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid3 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid4 = IdGenerator.generateTimeBasedUUID();
+
+        System.out.println("UUID 1: " + uuid1);
+        System.out.println("UUID 2: " + uuid2);
+        System.out.println("UUID 3: " + uuid3);
+        System.out.println("UUID 4: " + uuid4);
+
+        System.out.println("UUID val 1: " + UUIDv7Utils.extractOffsetDateTime(uuid1));
+        System.out.println("UUID val 2: " + UUIDv7Utils.extractOffsetDateTime(uuid2));
+        System.out.println("UUID val 3: " + UUIDv7Utils.extractOffsetDateTime(uuid3));
+        System.out.println("UUID val 4: " + UUIDv7Utils.extractOffsetDateTime(uuid4));
+
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid1).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertEquals(currentDateTime, uuidDateTime);
+    }
+
+}
