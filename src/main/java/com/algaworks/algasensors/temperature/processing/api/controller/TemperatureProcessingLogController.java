5ff3bca428fd65ec3a5ce56f33314caa4b954578
jucami90 <jucami90@gmail.com>
2026-03-10 12:25:16 -0300

    add rabbit improvements

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingLogController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingLogController.java
@@ -5,6 +5,7 @@ import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -49,6 +50,11 @@ public class TemperatureProcessingLogController {
         String routingKey = "";
         Object payload = logOutput;
 
-        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+        MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", logOutput.getSensorId().toString());
+            return message;
+        };
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload, messagePostProcessor);
     }
 }
